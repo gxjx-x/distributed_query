3487c94bbb4578198349be089fa7839f5089a724
darko-mesaros <darko.subotica@gmail.com>
2025-05-28 14:45:51 -0700

    Initial commit

---
+++ test/distributed_query.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DistributedQuery from '../lib/distributed_query-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/distributed_query-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DistributedQuery.DistributedQueryStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
